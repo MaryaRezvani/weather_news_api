fa2e08b9108cdcbaa56b77362c9d153753bbf261
maryam rezvani <rezvani.maryam.402@gmail.com>
2024-10-29 21:47:51 +0330

    editing settings.py for restframework

--- weather_news_api/settings.py
+++ weather_news_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v^y%w8-vvrozgicx%k0awvn!hf%iw6t&3v_$o53mt2mcq4$jh8"
+SECRET_KEY = "django-insecure-ly%jucry^-pb9(*)wt1bug7de7hlykae^joq-gc+4(q3-1p@v0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "djangorestframework",
+    'rest_framework',
     "news_weather",
 ]
 
@@ -82,6 +82,41 @@ DATABASES = {
     }
 }
 
+# تنظیمات REST Framework
+REST_FRAMEWORK = {
+    # برای کنترل مجوزهای پیش‌فرض
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # درخواست‌ها فقط توسط کاربران احراز هویت شده انجام شوند
+    ],
+    # برای کنترل روش احراز هویت پیش‌فرض
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',  # احراز هویت با استفاده از سشن‌ها
+        'rest_framework.authentication.TokenAuthentication',    # احراز هویت با استفاده از توکن‌ها
+    ],
+
+    # تنظیمات محدودیت سرعت (Throttle)
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',  # محدودیت سرعت برای کاربران وارد شده
+        'rest_framework.throttling.AnonRateThrottle',  # محدودیت سرعت برای کاربران ناشناس
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '1000/day',   # هر کاربر می‌تواند حداکثر 1000 درخواست در روز ارسال کند
+        'anon': '100/day',    # هر کاربر ناشناس حداکثر 100 درخواست در روز می‌تواند ارسال کند
+    },
+
+    # تنظیمات کلاس پیش‌فرض برای Pagination (صفحه‌بندی)
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # تعداد آیتم‌ها در هر صفحه
+
+    # تنظیمات رندرر (Renderer)
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',  # پاسخ‌دهی در قالب JSON
+        # 'rest_framework.renderers.BrowsableAPIRenderer',  # رابط وب قابل مشاهده (برای محیط توسعه مناسب است)
+    ],
+
+    # تنظیمات برای مستندسازی API
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
